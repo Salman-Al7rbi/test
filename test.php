ff6fd02d2eb9f19417318c6238005f838d307155
سلمان الحربي <dolar1416@gmail.com>
2017-05-26 02:12:17 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,4 +1,7 @@
- <?php
-// by ElSyad :)
-file_put_contents('basel.php',file_get_contents('https://raw.githubusercontent.com/Salman-Al7rbi/test/master/web.php'));
-?> 
+
+<?php
+
+// Show all information, defaults to INFO_ALL
+phpinfo();
+
+?>
